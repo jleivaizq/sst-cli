b233c07df2f0f727e6f30807a84e591a92618c1c
Jose A. Leiva <jleivaizq@gmail.com>
2021-04-25 20:21:23 +0200

    Initial CLI params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use structopt::StructOpt;
+use log::{info};
+
+#[derive(Debug)]
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short = "s", long = "symbols")]
+    symbols: Vec<String>,
+    #[structopt(short = "f", long = "from")]
+    from: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Cli::from_args();
+    info!("starting up");
+    println!("{:?}", args);
 }
